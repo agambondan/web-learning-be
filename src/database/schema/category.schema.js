fbb2afedb8f98faf0a666a4575761771379d0782
agambondan <agam.pro234@gmail.com>
2019-11-29 23:34:54 +0700

    [FEATURE] ADD MODULE CATEGORY

---
+++ src/database/schema/category.schema.js
@@ -0,0 +1,30 @@
+import Category from '../../models/category.model';
+import {EntitySchema} from "typeorm";
+
+const categorySchema = new EntitySchema({
+    name: 'Category',
+    target: Category,
+    tableName: 'categories',
+    columns: {
+        id: {
+            primary: true,
+            type: 'uuid',
+            generated: 'uuid',
+        },
+        category: {
+            type: 'varchar',
+            unique: true,
+            nullable: true,
+        },
+    },
+    relations: {
+        articles: {
+            target: 'Articles',
+            type: 'one-to-many',
+            joinColumn: true,
+            eager: true,
+        }
+    }
+});
+
+export default categorySchema;
